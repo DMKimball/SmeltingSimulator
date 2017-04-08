498c9cf8f2d3507dd2e8ce36f1e597b270b4bb9d
Dana Kimball <dkimball@ucsd.edu>
2017-04-08 16:23:02 -0700

    Basic Movemnet/Grabbing online

---
+++ Assets/CustomScripts/PC_Grab.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Grab : MonoBehaviour {
+
+    public enum HandSide { left, right };
+
+    public HandSide side;
+
+    private Collider hand_collider;
+    private Rigidbody rbody;
+
+    private GameObject grabbableObject;
+    private bool grabbing;
+
+	// Use this for initialization
+	void Awake () {
+        hand_collider = GetComponent<Collider>();
+        rbody = GetComponent<Rigidbody>();
+        grabbing = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!grabbing && grabbableObject != null && ((side == HandSide.right && OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0) || (side == HandSide.left && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0)))
+        {
+            grabbing = true;
+            FixedJoint joint = grabbableObject.AddComponent<FixedJoint>();
+            joint.connectedBody = rbody;
+        }
+
+        if (grabbing && ((side == HandSide.right && OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) == 0) || (side == HandSide.left && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) == 0)))
+        {
+            grabbing = false;
+            FixedJoint joint = grabbableObject.GetComponent<FixedJoint>();
+            Destroy(joint);
+            grabbableObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity((side == HandSide.left) ? OVRInput.Controller.LTouch : OVRInput.Controller.RTouch);
+            grabbableObject = null;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Collided with object");
+        if (grabbing) return;
+        if (other.gameObject.GetComponent<Rigidbody>() != null && other.tag.Equals("Grabbable"))
+        {
+            //Debug.Log("Found grabbable object");
+            grabbableObject = other.gameObject;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (grabbing) return;
+        if (other.gameObject == grabbableObject) grabbableObject = null;
+    }
+}
---
+++ Assets/CustomScripts/PC_Movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Movement : MonoBehaviour {
+
+    private Rigidbody rbody;
+    public Transform referenceCam;
+    public Transform player;
+
+    public float moveSpeed = 5.0f;
+    public float jumpSpeed = 1.0f;
+    public float jumpWait = 0.5f;
+    public float teleportWait = 0.5f;
+    public float verticalOffset = 0.5f;
+
+    private bool jumpReady;
+    private bool teleportReady;
+
+	// Use this for initialization
+	void Awake () {
+        rbody = GetComponent<Rigidbody>();
+        teleportReady = jumpReady = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 lStickPos = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        float xMove = -lStickPos.x;
+        float zMove = lStickPos.y;
+
+        Vector3 forward = referenceCam.forward;
+        Vector3 right = Vector3.Cross(forward, Vector3.up);
+
+        Vector3 velocity = Vector3.Normalize(forward * zMove + right * xMove) * moveSpeed;
+
+        velocity.y = (OVRInput.Get(OVRInput.Button.One) && jumpReady) ? jumpSpeed : rbody.velocity.y;
+        if (OVRInput.Get(OVRInput.Button.One) && jumpReady) StartCoroutine("JumpCooldown");
+
+        rbody.velocity = velocity;
+
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick) && teleportReady)
+        {
+            player.position = transform.position + Vector3.up * verticalOffset;
+            StartCoroutine("TeleportCooldown");
+        }
+    }
+
+    IEnumerator JumpCooldown()
+    {
+        jumpReady = false;
+        yield return new WaitForSeconds(jumpWait);
+        jumpReady = true;
+        yield break;
+    }
+
+    IEnumerator TeleportCooldown()
+    {
+        teleportReady = false;
+        yield return new WaitForSeconds(teleportWait);
+        teleportReady = true;
+        yield break;
+    }
+}
